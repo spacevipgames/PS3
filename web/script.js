37c7a890a90659a443c951c77322321f2c90b8b4
spacevipgames <132319184+spacevipgames@users.noreply.github.com>
2025-03-21 07:06:18 -0300

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,42 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function loadList(file) {
-        fetch(file)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Lista não encontrada.');
-                }
-                return response.text();
-            })
-            .then(html => {
-                document.getElementById("listContainer").innerHTML = html;
-                attachDownloadListeners();
-            })
-            .catch(error => {
-                document.getElementById("listContainer").innerHTML = "<p>Lista não encontrada.</p>";
-            });
-    }
-
-    function attachDownloadListeners() {
-        document.querySelectorAll(".download-link").forEach(link => {
-            link.addEventListener("click", function (event) {
-                event.preventDefault();
-                const url = this.getAttribute("href");
-                window.location.href = url;
-            });
-        });
-    }
-
-    document.querySelectorAll(".list-link").forEach(link => {
+    var links = document.querySelectorAll(".list-link");
+    links.forEach(function (link) {
         link.addEventListener("click", function (event) {
             event.preventDefault();
-            const file = this.getAttribute("data-file");
-            loadList(file);
+            var file = this.getAttribute("data-file");
+            carregarLista(file);
         });
     });
-
-    // Se houver um hash na URL, carregar a lista correspondente
-    const initialFile = window.location.hash.replace("#", "");
-    if (initialFile) {
-        loadList(initialFile);
-    }
 });
+
+function carregarLista(file) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", file, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById("listContainer").innerHTML = xhr.responseText;
+        } else if (xhr.readyState == 4) {
+            document.getElementById("listContainer").innerHTML = "Erro ao carregar a lista.";
+        }
+    };
+    xhr.send();
+}
