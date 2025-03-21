8d083f46a1e8f0fdcb6b69c26f4a506e7821a6ca
spacevipgames <132319184+spacevipgames@users.noreply.github.com>
2025-03-21 08:51:47 -0300

    Update script.js

--- web/script.js
+++ web/script.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const listSelection = document.getElementById("listSelection");
     const listContainer = document.getElementById("listContainer");
 
+    // Carregar a lista automaticamente ao iniciar a página
+    loadList("a1.html");
+
     // Carregar a lista quando um link for clicado
     document.querySelectorAll(".list-link").forEach(link => {
         link.addEventListener("click", function (event) {
@@ -31,14 +34,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function attachDownloadLinks() {
         document.querySelectorAll(".download-link").forEach(link => {
+            let archiveFile = link.getAttribute("href").trim();
+            
+            if (!archiveFile.startsWith("http")) {
+                archiveFile = `https://dn721001.ca.archive.org/0/items/sony_playstation3_a_part1/${encodeURIComponent(archiveFile)}`;
+            }
+            
+            link.setAttribute("href", archiveFile);
+            link.setAttribute("download", ""); // Força o download automático
             link.addEventListener("click", function (event) {
                 event.preventDefault();
-                let archiveFile = this.getAttribute("href").trim();
-                
-                if (!archiveFile.startsWith("http")) {
-                    archiveFile = `https://dn721001.ca.archive.org/0/items/sony_playstation3_a_part1/${encodeURIComponent(archiveFile)}`;
-                }
-                
                 window.location.href = archiveFile;
             });
         });
